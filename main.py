07af34101f827025c57fdb51fc515cac42691755
alyush <120322428+alyush@users.noreply.github.com>
2022-12-13 23:04:54 +0500

    Создание меню
    Это было последним этапом кодирования, надеюсь.
    
    Тут был написано старт меню и логичный конец игры.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ pygame.init()
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 RUNNING = [pygame.image.load(os.path.join("Assets/Dino", "DinoRun1.png")),
            pygame.image.load(os.path.join("Assets/Dino", "DinoRun2.png"))]
 JUMPING = pygame.image.load(os.path.join("Assets/Dino", "DinoJump.png"))
@@ -154,7 +153,7 @@ class Bird(Obstacle):
     def __init__(self, image):
         self.type = 0
         super().__init__(image, self.type)
-        self.rect.y = 250
+        self.rect.y = 260
         self.index = 0
 
     def draw(self, SCREEN):
@@ -175,6 +174,7 @@ def main():
     points=0
     font=pygame.font.Font('freesansbold.ttf', 20)
     obstacles=[]
+    death_count=0
 
     def score():
         global points, game_speed
@@ -221,7 +221,9 @@ def main():
             obstacle.draw(SCREEN)
             obstacle.update()
             if player.dino_rect.colliderect (obstacle.rect):
-                pygame.draw.rect(SCREEN, (250,0,0), player.dino_rect, 2)
+                pygame.time.delay(2000)
+                death_count+=1
+                menu(death_count)
 
 
 
@@ -236,4 +238,40 @@ def main():
         pygame.display.update()
 
 
-main()
+def menu(death_count):
+    global points
+    run = True
+    while run:
+        SCREEN.fill((255, 255, 255))
+        font = pygame.font.Font('freesansbold.ttf', 30)
+
+        if death_count == 0:
+            text = font.render("Press any Key to Start", True, (0, 0, 0))
+        elif death_count > 0:
+            text = font.render("Press any Key to Restart", True, (0, 0, 0))
+            score = font.render("Your Score: " + str(points), True, (0, 0, 0))
+            scoreRect = score.get_rect()
+            scoreRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
+            SCREEN.blit(score, scoreRect)
+        textRect = text.get_rect()
+        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        SCREEN.blit(text, textRect)
+        SCREEN.blit(RUNNING[0], (SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT // 2 - 140))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                main()
+
+
+menu(death_count=0)
+
+
+
+
+
+
+
+
+
